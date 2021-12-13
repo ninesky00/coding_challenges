6e4dbdf08a33af8298febd7a284378f04f67f158
ninesky00 <jiangzho@gmail.com>
2021-12-13 14:37:01 -0600

    add coin_change.rb

---
+++ coin_change.rb
@@ -0,0 +1,60 @@
+require 'pry'
+# 322. Coin Change
+# You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+
+# Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+
+# You may assume that you have an infinite number of each kind of coin.
+
+
+# Example 1:
+
+# Input: coins = [1,2,5], amount = 11
+# Output: 3
+# Explanation: 11 = 5 + 5 + 1
+# Example 2:
+
+# Input: coins = [2], amount = 3
+# Output: -1
+# Example 3:
+
+# Input: coins = [1], amount = 0
+# Output: 0
+# Example 4:
+
+# Input: coins = [1], amount = 1
+# Output: 1
+# Example 5:
+
+# Input: coins = [1], amount = 2
+# Output: 2
+ 
+
+# Constraints:
+
+# 1 <= coins.length <= 12
+# 1 <= coins[i] <= 231 - 1
+# 0 <= amount <= 104
+
+
+def coin_change(coins, amount)
+  dp = [amount + 1] * (amount + 1)
+  #setting the array to maximum possible combinations to reach amount + 1
+  dp[0] = 0
+  for a in 0...amount+1
+    for c in coins
+      #looping through every number and coins to filter how many minimum coins are needed to add to current number
+      # the array is storing each previous coins required to make an amount, to make the search faster. 
+      if a - c >= 0
+        dp[a] = [dp[a], 1 + dp[a-c]].min
+      end
+    end
+  end
+  #checking the if the array was altered, if it was not then we could not find the solution. 
+  dp[amount] != amount + 1 ? dp[amount] : -1
+end
+
+
+# puts coin_change([1,2,5], 11)
+puts coin_change([2], 3)
+puts coin_change([1], 0)
